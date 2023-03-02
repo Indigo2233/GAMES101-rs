fb1b8453236dac223ca0ab134b797ec07a079936
Indigo <ysu@smail.nju.edu.cn>
2023-03-02 14:45:29 +0800

    run gcc

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 动态链接了Clib里包装为C库的C++库.
 
-动态库的编译：`g++ -shared -o libobjloader.so OBJ_Loader_C.cpp -lc -fPIC`.
+动态库的编译：`g++ -shared -o libobjloader.so OBJ_Loader_C.cpp -lc -fPIC`, 已经在`build.rs`中给出.
 
 运行时需要配置环境变量：`export LD_LIBRARY_PATH=./Clib`.
 
--- build.rs
+++ build.rs
@@ -4,5 +4,7 @@ fn main() {
     println!(r"cargo:rustc-link-search=native=Clib");
     println!("cargo:rerun-if-changed=Clib/OBJ_Loader.h");
     println!("cargo:rerun-if-changed=Clib/OBJ_Loader_C.cpp");
-    Command::new("g++").args(["-shared", "-o", "Clib/libobjloader.so", "Clib/OBJ_Loader_C.cpp", "-lc", "-fPIC"]);
+    Command::new("g++")
+        .args(["-shared", "-o", "Clib/libobjloader.so", "Clib/OBJ_Loader_C.cpp", "-lc", "-fPIC"]).
+        spawn().unwrap();
 }
